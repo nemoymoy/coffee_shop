0c9e07432215d42b45563f1e0de662465a6c9cca
nemoymoy <nemoymoy@gmail.com>
2026-08-12 00:06:24 +0300

    fix(settings): correct TEMPLATES DIRS path to find templates in app subdirectory

--- django/coffee_shop/settings/base.py
+++ django/coffee_shop/settings/base.py
@@ -78,7 +78,7 @@ ROOT_URLCONF = 'coffee_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'coffee_shop' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
