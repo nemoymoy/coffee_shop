ca0ae0491541638fbe0135160e4c2dcb5117ba90
Nemo <nemo@coffee.shop>
2026-08-29 14:10:40 +0300

    feat: добавлен вес и размеры посылки в лог расчета доставки
    Теперь при расчете стоимости доставки в консоль выводится:
    - Вес посылки (товар + тара)
    - Размеры посылки (длина x ширина x высота)

--- django/static/js/yandex_delivery_modal.js
+++ django/static/js/yandex_delivery_modal.js
@@ -227,7 +227,11 @@ const YandexDeliveryWidget = (() => {
             const totalProductWeight = cartState.items.reduce((sum, item) => sum + item.weight * item.quantity, 0);
             const tare = findPackageForWeight(totalProductWeight);
             const tareWeightGrams = tare ? parseFloat(tare.tare_weight) * 1000 : 0;
-            console.log('[YandexDelivery] Вес тары:', tareWeightGrams, 'г');
+            const totalPackageWeightGrams = totalProductWeight + tareWeightGrams;
+            console.log('[YandexDelivery] Вес посылки:', totalPackageWeightGrams, 'г');
+            if (tare) {
+                console.log('[YandexDelivery] Размеры посылки:', `${tare.length}x${tare.width}x${tare.height} м`);
+            }
             console.log('[YandexDelivery] ==========================================');
 
             const data = await apiPost(CONFIG.CALCULATE_DELIVERY_URL, payload);
